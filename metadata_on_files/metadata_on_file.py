0a75d3cee1ce0b61e41e5cbf658cf6d5f31f8250
orel <0533orel@gmail.com>
2025-09-07 15:54:27 +0300

    Added a method that accepts a list of dictionaries and returns a list of jsons

--- metadata_on_files/metadata_on_file.py
+++ metadata_on_files/metadata_on_file.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import datetime
+import json
 
 
 # Reminder to add validation and docstring
@@ -17,12 +18,21 @@ class MetadataOnFiles:
             current_datetime = datetime.now()
             last_modified_timestamp = file.stat().st_mtime
             list_of_metadata.append({
-                "file name": file.stem,
-                "type": file.suffix,
-                "path": file.parent,
-                "size": file.stat().st_size,
+                "file name": str(file.stem),
+                "type": str(file.suffix),
+                "path": str(file.parent),
+                "size in bytes ": file.stat().st_size,
                 "creation date": current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
                 "Last modified date": datetime.fromtimestamp(last_modified_timestamp).strftime("%Y-%m-%d %H:%M:%S")
             })
 
         return list_of_metadata
+
+    def get_list_of_jsons(self, list_of_metadata: list[dict]):
+        json_list = []
+        for metadata in list_of_metadata:
+            json_string = json.dumps(metadata, indent=6)
+            json_list.append(json_string)
+
+        return json_list
+
