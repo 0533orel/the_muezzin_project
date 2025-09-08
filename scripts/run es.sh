39c97de9be18cd84345a87ebdd0c06aa2357164f
orel <0533orel@gmail.com>
2025-09-08 15:19:21 +0300

    Added kibana to docker compose and the ID of each document changed to hash instead of uuid

--- scripts/run es.sh
+++
@@ -1,5 +0,0 @@
-docker run -d --name es \
-  -e "discovery.type=single-node" \
-  -e "xpack.security.enabled=false" \
-  -p 9200:9200 -p 9400:9400 \
-  docker.elastic.co/elasticsearch/elasticsearch:8.15.0
